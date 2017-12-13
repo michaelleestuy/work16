1571f6a4337a98b8904e67ec3ab146717b8ba731
matteowong <mwong8@stuy.edu>
2017-12-13 11:24:23 -0500

    finished mod convo

--- basic_server.c
+++ basic_server.c
@@ -1,5 +1,13 @@
 #include "pipe_networking.h"
+#include <string.h>
 
+void to_upper(char * s) {
+
+  int i=0;
+  while (i<strlen(s))
+    s[i++]-=32;
+
+}
 
 int main() {
 
@@ -7,4 +15,16 @@ int main() {
   int from_client;
 
   from_client = server_handshake( &to_client );
+
+  while (1) {
+
+    char s[256];
+    read(from_client,s,256);
+    printf("Modifying string\n");
+    to_upper(s);
+    write(to_client,s,256);
+
+
+
+  }
 }
--- client.c
+++ client.c
@@ -7,4 +7,18 @@ int main() {
   int from_server;
 
   from_server = client_handshake( &to_server );
+
+  while (1) {
+
+    char s[256];
+    printf("Input a string (all letters, all lowercase): ");
+    fgets(s,256,stdin);
+    //printf("input read in to s: [%s]\n",s);
+    write(to_server,s,256);
+    //printf("wrote text\n");
+    read(from_server,s,256);
+    printf("Received modified string: [%s]\n",s);
+
+
+  }
 }
